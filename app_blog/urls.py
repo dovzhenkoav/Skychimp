7e826fa8a52c84e7bcabfa725b1ce5b77563340f
OFAxTAB <rockenny@ymail.com>
2023-08-14 20:42:40 +0300

    add blog_list

--- app_blog/urls.py
+++ app_blog/urls.py
@@ -6,5 +6,5 @@ from app_blog import views
 app_name = 'app_blog'
 
 urlpatterns = [
-    path('blog/', views.BlogListView.as_view(), name='blog_list'),
+    path('', views.BlogListView.as_view(), name='blog_list'),
 ]
