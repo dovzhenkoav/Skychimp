36a64c13a865af8c4adf7bf38a93e1ce53cac3e8
OFAxTAB <rockenny@ymail.com>
2023-08-14 13:06:49 +0300

    add login&logout for users

--- app_users/urls.py
+++ app_users/urls.py
@@ -1,7 +1,14 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+
+#from app_users.views import RegisterView, verify_view, forget_email_view, recover_password_confirmation_view
 
 app_name = 'app_users'
 
 urlpatterns = [
+    path('login/', LoginView.as_view(template_name='app_users/login.html'), name='login'),
+
+    path('logout/', LogoutView.as_view(), name='logout'),
+
 
 ]
