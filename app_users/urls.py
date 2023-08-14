a927f1a97ffe55e3ba6576e41d68dc1582bc56dd
OFAxTAB <rockenny@ymail.com>
2023-08-14 13:45:01 +0300

    add mailing service settings

--- app_users/urls.py
+++ app_users/urls.py
@@ -7,8 +7,10 @@ app_name = 'app_users'
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='app_users/login.html'), name='login'),
-
+    # path('verify/<int:code>', verify_view, name='verify'),
     path('logout/', LogoutView.as_view(), name='logout'),
-
+    # path('register/', RegisterView.as_view(), name='register'),
+    # path('forget_email/', forget_email_view, name='forget_email'),
+    # path('recover_conf/', recover_password_confirmation_view, name='recover_password')
 
 ]
